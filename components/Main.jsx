e9c696d1b5aa3b1b4b08a37f2d146fccc69112be
rahulgore54 <108347282+rahulgore54@users.noreply.github.com>
2022-11-01 23:34:31 +0530

    Update Main.jsx

--- components/Main.jsx
+++ components/Main.jsx
@@ -10,7 +10,7 @@ const Main = () => {
             <div className='max-w-[1240px] w-full h-full mx-auto p-2 flex justify-center items-center'>
                 <div>
                     <p className='uppercase text-sm tracking-widest text-gray-600'>Let's Build Something Together</p>
-                    <h1 className='py-4 text-gray-700'>Hi,I'm <span className='text-[#5651e5]'>Rahul</span></h1>
+                    <h1 className='py-4 text-gray-700'>Hi,I'm <span className='text-[#5651e5]'>Vaishnavi</span></h1>
                     <h1 className='py-4 text-gray-700'>A Front-End Web Developer</h1>
                     <p className='py-4 text-gray-600 max-w-[70%] m-auto'>I'm a front-end web developer specializing in building (and ocassionally designing) exceptional digital experiences. Currently I'm focused on building responsive front-end web applications while learning back-end technologies.</p>
 
